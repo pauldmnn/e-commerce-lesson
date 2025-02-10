463efb66585803692a283067c3362d36a54fa8f6
pauldmnn <pauldmnn@gmail.com>
2025-02-10 12:20:42 +0000

    Updated allauth templates and test user profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+
+from .models import UserProfile
 # Create your views here.
+
 
 def profile(request):
     """
-    Display the user profile"""
+    Display the user profile
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
